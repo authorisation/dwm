fac4d18c911645a666d54bcce7d249a834cd1408
auth <64337177+authorisation@users.noreply.github.com>
2024-06-06 19:57:21 +0200

    feat: style

--- config.h
+++ config.h
@@ -12,16 +12,17 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;        /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
+static const char *fonts[]          = { "UbuntuMono Nerd Font:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_gray1[]       = "#1D2330";
+static const char col_gray2[]       = "#4B5665";
+static const char col_gray3[]       = "#AD69AF";
+static const char col_gray4[]       = "#d1d7e1";
+static const char col_cyan[]        = "#84598D";
+
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
+	[SchemeNorm] = { col_gray4, col_gray1, col_gray2 },
 	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
 };
 
@@ -132,7 +133,6 @@ static const Key keys[] = {
     { MODKEY|ShiftMask,             XK_q,      killclient,     {0} },
 
 	{ MODKEY|ShiftMask,             XK_e,      exitdwm,       {0} },
-	{ MODKEY,                       XK_o,      winview,        {0} },
 	{ MODKEY,                     XK_Tab,    alttab,         {0} },
 
     TAGKEYS(                            XK_1,                       0)
